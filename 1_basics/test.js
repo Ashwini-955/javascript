7640a532ea3fbb84ba542348d6a876d73a407eba
Ashwini-955 <ashwinirm052@gmail.com>
2025-05-02 11:45:07 +0530

    changes

---
+++ 1_basics/test.js
@@ -0,0 +1,8 @@
+console.log("hello")
+const accountid = 1234
+let email ="ashwini@gmail.com"
+var password = "123456"
+city ="kalyan"
+let state;
+console.table([accountid,email,password,city,state])
+console.log(typeof "email")
