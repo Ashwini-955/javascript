c7f01aa5f56d0ffff65ff6edad8ac6b6bba50c36
Ashwini955 <ashwinirm052@gmail.com>
2026-01-30 22:48:44 +0530

    .

---
+++ 04_flowControl/nullish.js
@@ -0,0 +1,11 @@
+// nullish coallescing operator ?? 
+let val1;
+let val2;
+val1=5??10 // this will take first num as input 
+val2=null??2 // here first is null so it will avoid null and take second as input
+// this is used to avoid the disturbance of code if any null value comes from api or database
+console.log(val1) //5
+console.log(val2)//2 instead of null
+
+// ternary operator 
+condition? true :false
---
+++ 04_flowControl/truthy.js
@@ -0,0 +1,16 @@
+/* 
+true
+1
+-1
+"0"
+"false"
+" "        // space
+[]
+{}
+function(){}
+Infinity
+if ("false")   // true
+if ("0")       // true
+if ([])        // true
+if ({})        // true
+*/
